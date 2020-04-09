47cf03fa8404efc9251a927bdfeeb6e339811be7
Denis Molloy <denis.molloy@gmail.com>
2020-04-08 23:19:35 -0700

    implemented game logic

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,21 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import WobbleCard from '../../components/WobbleCard';
 import familyData from '../../data.json';
 import './home.css';
 
 export default function Home() {
-  const renderCards = () =>
-    familyData.map((person) => (
-      <WobbleCard key={person.id} person={person} handleClick={handleClick} />
-    ));
+  const [people, setPeople] = useState(familyData);
+  const [score, setScore] = useState(0);
+  const [topScore, setTopScore] = useState(0);
 
-  const handleClick = (id) => {
-    console.log('CLICK ID - ', id);
+  const handleCorrectGuess = (newData) => {
+    const newScore = score + 1;
+    const newTopScore = Math.max(newScore, topScore);
+
+    setScore(newScore);
+    setTopScore(newTopScore);
+    setPeople(shuffleData(newData));
   };
 
+  const handleIncorrectGuess = (data) => {
+    setScore(0);
+    setPeople(resetData(data));
+  };
+
+  const resetData = (data) => {
+    const resetData = data.map((item) => ({ ...item, clicked: false }));
+    return shuffleData(resetData);
+  };
+
+  const shuffleData = (data) => {
+    let i = data.length - 1;
+    while (i > 0) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = data[i];
+      data[i] = data[j];
+      data[j] = temp;
+      i--;
+    }
+    return data;
+  };
+
+  const handleCardClick = (id) => {
+    let guessedCorrectly = false;
+    const newData = people.map((item) => {
+      const newItem = { ...item };
+      if (newItem.id === id) {
+        if (!newItem.clicked) {
+          newItem.clicked = true;
+          guessedCorrectly = true;
+        }
+      }
+      return newItem;
+    });
+    guessedCorrectly
+      ? handleCorrectGuess(newData)
+      : handleIncorrectGuess(newData);
+  };
+
+  const renderCards = () =>
+    people.map((person) => (
+      <WobbleCard
+        key={person.id}
+        person={person}
+        handleClick={handleCardClick}
+      />
+    ));
+
   return (
-    <div>
-      <h1>This is home</h1>
+    <div id='main'>
+      <h1>{`Score: ${score}  |  Top score: ${topScore}`}</h1>
       <div className='card-container'>{renderCards()}</div>
     </div>
   );
