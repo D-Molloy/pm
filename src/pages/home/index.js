e8b58769bcac2da7037f72c861b0d3c2f49415f3
Denis Molloy <denis.molloy@gmail.com>
2020-04-09 00:01:22 -0700

    added animation

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import WobbleCard from '../../components/WobbleCard';
 import familyData from '../../data.json';
 import './home.css';
@@ -7,6 +7,10 @@ export default function Home() {
   const [people, setPeople] = useState(familyData);
   const [score, setScore] = useState(0);
   const [topScore, setTopScore] = useState(0);
+
+  // useEffect(() => {
+  //   document.querySelector('.App').classList.toggle('element');
+  // }, []);
 
   const handleCorrectGuess = (newData) => {
     const newScore = score + 1;
@@ -67,6 +71,24 @@ export default function Home() {
 
   return (
     <div id='main'>
+      {/* TODO: move background animation to it's own component */}
+      {/* TODO: move styles to css */}
+      <div
+        className='element'
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100vw',
+          height: '100vh',
+          zIndex: -1,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <h1>HELLO WORLD</h1>
+      </div>
       <h1>{`Score: ${score}  |  Top score: ${topScore}`}</h1>
       <div className='card-container'>{renderCards()}</div>
     </div>
